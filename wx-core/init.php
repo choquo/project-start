15b93ff918244ec95f8a4405ca5d914d5d2d423d
Carlos Maldonado <karlozm@gmail.com>
2016-03-29 13:01:22 -0600

    bugfix routes
    fix to routes for work on localhost and online, with a test the online
    routes wasn't working, after this fix routes work fine online and in
    localhost

--- wx-core/init.php
+++ wx-core/init.php
@@ -10,9 +10,10 @@ require $core_dirname.'/functions-built-in.php';
 //Put your database user and password into admin/conex.php
 $install_dirname 	= dirname($_SERVER['PHP_SELF']); //The dirname where app is running, this facilitate the change of the website folder without headaches, just move to another folder and your website will be ready automatically.
 
-//Prepare routes
+//Prepare routes (url_params are required by is_hompage() function )
 $requested_url_params = array();
 $requested_url = str_replace($install_dirname, '', $_SERVER['REQUEST_URI']); //Remove install dirname to get the real requested url: domain-name-com/[this] or domain-name-com/installdirname/[this]
+$requested_url = '/'.ltrim($requested_url,'/'); //Fix to work on localhost and online routes (remove /// and leave only one slash / )
 $requested_url_params = array_merge(array_filter(explode('/',$requested_url)));
 
 //Load content
