5e88b5d34e1a3e684df6f0b3234ea3b1eec73c25
Carlos Maldonado <karlozm@gmail.com>
2016-05-07 18:07:54 -0500

    minor changes

--- _config.php
+++ _config.php
@@ -2,16 +2,11 @@
 $_config = true; //For validate !isset($_config) in external includes if var is not present then include this file "_config.php"
 
 /**
-* Set variables to use globally
+* Set constants to use globally
 */
 define('ADMIN_DIRNAME', 'admin');
 define('CORE_DIRNAME', 'core');
 define('INCLUDES_DIRNAME', 'includes');
 define('UPLOADS_URL', ADMIN_DIRNAME.'/uploads');
-
-//$admin_dirname		= 'admin';
-//$core_dirname 		= 'wx-core';
-//$includes_dirname 	= 'wx-includes';
-//$uploads_url 		= $admin_dirname.'/uploads';
 
 ?>
